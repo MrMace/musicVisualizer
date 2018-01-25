fecf3fb894a455c263de7d9dc345f245372ad7cf
mattt <mace.matt@umail.iu.edu>
2018-01-25 17:24:17 -0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Matt
+ * Date: 1/25/2018
+ * Time: 4:38 PM
+ */
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+  <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+
+
+<audio id="audio" src="" autoplay loop></audio>
+
+<canvas id="visualizer"></canvas>
+
+<div id="controls">
+    <img src="icons/pause.png">
+</div>
+
+
+<script src="js.js"><</script>
+</body>
+</html>
+
+
+
---
+++ js.js
@@ -0,0 +1,87 @@
+// Setup Canvas
+var canvas = document.getElementById('visualizer');
+var canvasContext = canvas.getContext('2d');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight / 2;
+
+// Setup Audio Context
+var audioContext = new AudioContext();
+var audio = document.getElementById('audio');
+
+var source = audioContext.createMediaElementSource(audio);
+var analyser = audioContext.createAnalyser();
+
+source.connect(analyser);
+analyser.connect(audioContext.destination);
+
+var bufferLength = analyser.frequencyBinCount;
+var frequencyData = new Uint8Array(bufferLength);
+
+// Visualizer Settings
+function Visualizer() {
+    canvasContext.clearRect(0, 0, canvas.width, canvas.height);
+    analyser.getByteFrequencyData(frequencyData);
+
+    var frequencyWidth = (canvas.width / bufferLength),
+        frequencyHeight = 150,
+        x = 15;
+
+    for (var increment = 0; increment < bufferLength; increment++) {
+        frequencyHeight = frequencyData[increment] * (canvas.height * 0.002);
+        canvasContext.fillStyle = '#8719ff';
+        canvasContext.fillRect(x, canvas.height - frequencyHeight, frequencyWidth, frequencyHeight);
+        x += frequencyWidth + 15;
+    }
+
+    call = requestAnimationFrame(Visualizer);
+}
+
+// Default Audio Variable
+var isPlaying = false;
+
+// Start Visualizer on Load
+var startVisualizer = function() {
+    isPlaying = !isPlaying;
+    Visualizer();
+};
+startVisualizer();
+
+// Audio and Visualizer Controls
+var controls = document.getElementById('controls');
+
+controls.addEventListener('click', function() {
+    isPlaying = !isPlaying;
+
+    if (isPlaying) {
+        controls.innerHTML = "<img src='icons/pause.png'>";
+        controls.style.background = "#800080";
+        audio.play();
+        Visualizer();
+    } else {
+        controls.innerHTML = "<img src='icons/play.png'>";
+        controls.style.background = "#150817";
+        audio.pause();
+        cancelAnimationFrame(call);
+        canvas.height = window.innerHeight / 2;
+    }
+});
+
+// Load the Audio
+var request = new XMLHttpRequest();
+
+request.open('GET', 'https://s3-us-west-2.amazonaws.com/harriscarney/audio/Foliation.mp3', true);
+request.responseType = 'blob';
+
+request.onload = function() {
+    audio.src = window.URL.createObjectURL(request.response);
+    console.log(request.response);
+};
+
+request.send();
+
+// Resize Canvas on Browser Resize
+window.addEventListener('resize', function() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight / 2;
+});
